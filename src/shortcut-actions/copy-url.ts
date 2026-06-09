358813e24c3269c9638f05795a90b1aa4a0c931d
Arjun Sharma <arjun.sharma10@outlook.com>
2026-06-09 19:41:06 +0400

    after much effort, the clipboard has been integrated. Why can't I just use writeText from the service worker man, smh security my ass

--- src/shortcut-actions/copy-url.ts
+++ src/shortcut-actions/copy-url.ts
@@ -2,10 +2,22 @@ export async function copyCurrentURL(): Promise<void> {
     let queryOptions: object = { active: true, lastFocusedWindow: true};
     const [activeTab]: chrome.tabs.Tab[] = await chrome.tabs.query(queryOptions);
     
-    if(!activeTab?.url) {
+    const url: string | undefined = activeTab?.url;
+    const id: number | undefined = activeTab?.id;
+
+    if(typeof url == "undefined" || typeof id == "undefined") {
         return;
     }
 
-    console.log(activeTab.url);
-    // await navigator.clipboard.writeText(tab.url);
+    console.log(url);
+    console.log(id);
+
+    chrome.scripting.executeScript({
+        target: {tabId: id}, 
+        func: (url: string) => {
+            navigator.clipboard.writeText(url);
+            // console.log(`is the copy the issue? ${url}`);
+        },
+        args: [url]
+    });
 }
