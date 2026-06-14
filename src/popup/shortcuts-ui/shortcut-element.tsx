ce1a728d1650d46bf45dfc6e0b91586917b3d13d
Arjun Sharma <arjun.sharma10@outlook.com>
2026-06-14 22:56:01 +0530

    Actually I think I'm done for the time being; front end is way too annoying.

--- src/popup/shortcuts-ui/shortcut-element.tsx
+++ src/popup/shortcuts-ui/shortcut-element.tsx
@@ -9,7 +9,6 @@ export default function ShortcutElement({ command }: Props) {
         <div className = "card">
             <div className = "name">{command.description ?? "Unnamed Command"}</div>
             <div className = "shortcut">{command.shortcut ?? "Unassigned"}</div>
-            <input type="checkbox" className="shortcut-toggle"/>
         </div>
     )
 } 
